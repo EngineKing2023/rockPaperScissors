24957e3d8d6d8c4b6e6c2f3c17725cee334b0fe9
EngineKing2023 <engineking2023@gmail.com>
2025-01-20 22:13:38 +0100

    added playRound function

--- javascript.js
+++ javascript.js
@@ -2,29 +2,60 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(){
-    var randomNum = Math.floor(Math.random() * 3);
-    console.log(randomNum);
-    if(randomNum == 2){
+     computerScore = Math.floor(Math.random() * 3);
+    console.log(computerScore);
+ 
+    if(computerScore == 2){
         console.log("rock");
-    }else if(randomNum == 1){
+        return "rock"
+        
+    }else if(computerScore == 1){
         console.log("paper");
+        return "paper"
     }else{
         console.log("scissors");
+        return "scissors"
     }
     
 }
 
-getComputerChoice();
+
 
 function getHumanChoice(){
-    let person = prompt("Please enter your value","");
-    console.log(person);
-}
-
-getHumanChoice();
-
-function playRound(getHumanChoice,getComputerChoice){
     
-
+    let personInput = prompt("Please enter Rock, Paper or Scissors" ,"").toLowerCase();
+    
+     if(personInput ==="scissors"){
+        return "scissors"
+        
+        
+    } if(personInput ==="rock"){
+        return "rock"
+    } if(personInput ==="paper"){
+       return "paper"
+    }
+    else{
+        console.log( " typo problem");
+    }
 }
-playRound()
+
+
+
+function playRound(humanChoice, computerChoice) {
+if(humanChoice===computerChoice) {
+    console.log("draw");
+} else if (humanChoice === "paper" && computerChoice === "rock" ||
+             humanChoice ==="rock" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "paper"){
+    console.log("you win");
+}
+else {
+    console.log("you lose");
+}
+}
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+  
